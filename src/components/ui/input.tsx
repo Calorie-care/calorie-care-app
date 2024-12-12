ed413042b3eb0580495b5c18f0272e3e19926b36
Marcus Toledo <marcus_viniciustoledo@hotmail.com>
2024-12-11 21:48:49 -0300

    fix: Ajustado para centralizar do texto do input em iOS
    Necessário testar impactos no Android

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -42,17 +42,21 @@ const Input = React.forwardRef<
         </View>
         <View className="flex-row items-center mb-2 web:flex h-10 native:h-14 web:w-full rounded-md border border-input bg-background px-3">
           {icon && <View className="mr-2">{icon}</View>}
-          className={cn(
-            'mb-2 web:flex h-10 native:h-14 web:w-full rounded-md border border-input bg-background px-3 web:py-2 text-base lg:text-sm native:text-lg native:leading-[1.25] text-foreground placeholder:text-muted-foreground web:ring-offset-background file:border-0 file:bg-transparent file:font-medium web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
-            props.editable === false && 'opacity-50 web:cursor-not-allowed',
-            className
-          )}
-          placeholderClassName={cn(
-            'text-muted-foreground',
-            placeholderClassName
-          )}
-          {...props}
-        />
+          <TextInput
+            ref={ref}
+            className={cn(
+              'pb-2 text-base lg:text-sm native:text-lg placeholder:text-muted-foreground web:ring-offset-background file:border-0 file:bg-transparent file:font-medium web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
+              props.editable === false && 'opacity-50 web:cursor-not-allowed',
+              className
+            )}
+            multiline={true}
+            placeholderClassName={cn(
+              'text-muted-foreground',
+              placeholderClassName
+            )}
+            {...props}
+          />
+        </View>
       </>
     )
   }
