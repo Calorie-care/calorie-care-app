ed55704eb046da3c9a419113f34c5d9ad8a682c2
Marcus Toledo <marcus_viniciustoledo@hotmail.com>
2024-12-11 21:47:11 -0300

    feat: Alterado componente Input para receber um icon

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,6 +8,8 @@ interface CustomTextInputProps extends TextInputProps {
   label: string
   errorMessage?: string
   labelColor?: string
+  labelClassName?: string
+  icon?: React.ReactNode
 }
 
 const Input = React.forwardRef<
@@ -22,6 +24,7 @@ const Input = React.forwardRef<
       labelClassName,
       errorMessage,
       labelColor = 'text-foreground',
+      icon,
       ...props
     },
     ref
@@ -37,8 +40,8 @@ const Input = React.forwardRef<
           </Label>
           {errorMessage && <Text className="text-red-500">{errorMessage}</Text>}
         </View>
-        <TextInput
-          ref={ref}
+        <View className="flex-row items-center mb-2 web:flex h-10 native:h-14 web:w-full rounded-md border border-input bg-background px-3">
+          {icon && <View className="mr-2">{icon}</View>}
           className={cn(
             'mb-2 web:flex h-10 native:h-14 web:w-full rounded-md border border-input bg-background px-3 web:py-2 text-base lg:text-sm native:text-lg native:leading-[1.25] text-foreground placeholder:text-muted-foreground web:ring-offset-background file:border-0 file:bg-transparent file:font-medium web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
             props.editable === false && 'opacity-50 web:cursor-not-allowed',
